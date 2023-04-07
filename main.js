beca9fc9e1a1305400639c18c305b03f575d4ad2
Jason <jason5donoghue@gmail.com>
2023-04-07 13:02:11 +0100

    tests working and final solution removed

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
 /* 
-👉 Write your kata here!
+Frogland
+
+        In Frogland there are a diverse range of frogs. Due to the perfect climate conditions they thrive, BUT
+    there is one challenge the frogs must face each year.
+
+        The frogs must cross Frogland to access richer ponds as the season changes. On this path are THREE rivers
+    the frogs must cross to survive the rest of the year, and it is up to you to calculate HOW MANY FROGS REMAIN
+    from this tortuous journey.
+
+        Each type of frog has evolved differently, therefore they have different strengths at swimming. This means
+    there are different DEATH RATES for the frogs when crossing each river.
+
+    Brown Frog -    This dull coloured frog thrives in low water levels and loves splashing in muddy patches. They are
+                    poor swimmers resulting in them having an 8% DEATH RATE when crossing a river.
+
+    Green Frog -    Smaller and more energetic, these frogs like jumping through tall grass. This gives them great
+                    legs which they use for swimming strongly. Still they have a 5% DEATH RATE when crossing a river.
+
+    Gold Frog -     Finally these shiny and beautiful frogs are natures finest. They can jump high and swim the
+                    strongest. They are safe in the knowledge that they have a 1% DEATH RATE when crossing a river.
+
+        Now you know your frogs, given a random number of Brown, Green and Golden frogs, calculate how many survive
+    the crossing of the three rivers.
+
+        No half frogs survive, so if your TOTAL number comes to a floating point, it should be rounded up to an
+        integer as that partial frog will not survive. There will alway at least 1 or each frog type and there are
+        no negative frogs.
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+  function survivalOfTheFittest(brownFrog, greenFrog, goldFrog) {
+    //input code here to return the total number of frogs that survive the crossing of the three rivers
+    return totalSurvival;
+}
+module.exports = survivalOfTheFittest;
--- main.test.js
+++ main.test.js
@@ -1,1 +1,30 @@
 //👉 Write your tests below here:
+
+const survivalOfTheFittest = require('./main.js');
+
+//test to see if the function is defined
+test("survivalOfTheFittest is defined", () => {
+    expect(survivalOfTheFittest).toBeDefined();
+  });
+
+  //test to see if the function returns a number
+
+test("survivalOfTheFittest returns a number", () => {
+    expect(typeof survivalOfTheFittest(50, 25, 10)).toBe("number");
+  });
+
+  //test to see if the function returns the correct number
+
+test("survivalOfTheFittest returns the correct number", () => {
+    expect(survivalOfTheFittest(50, 25, 10)).toBe(68);
+  });
+
+  //test to see if the function returns the correct number
+
+test("survivalOfTheFittest returns the correct number", () => {
+    expect(survivalOfTheFittest(458, 235, 17)).toBe(564);
+  });
+
+test("survivalOfTheFittest returns the correct number", () => {
+    expect(survivalOfTheFittest(2357, 898, 133)).toBe(2683);
+  });
